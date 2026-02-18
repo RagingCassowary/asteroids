56b120869f59feb91182fab9065196dd57afab5b
RagingCassowary <jakeob@live.co.uk>
2026-02-18 19:14:43 +0000

    2: added constant.py and imported to main.py, updated main.py opening message

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pygame
+from constants.py import *
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}\n"
+          +
+          "Screen_height: {SCREEN_HEIGHT}"
+          )
 
 
 if __name__ == "__main__":
